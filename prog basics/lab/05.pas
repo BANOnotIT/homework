18dc8c16888d38dc7d881721a532bb2d188167f2
BANO.notIT <bano.notit@yandex.ru>
2019-09-16 22:52:52 +0300

    init: lab05

---
+++ prog basics/lab/05.pas
@@ -0,0 +1,29 @@
+program bobrenkoLab05;
+
+var
+  L : array[0..7] of array[0..5] of array[0..3] of integer;
+  i, j, a, b, c : integer;
+ 
+Begin
+  for a = 0 to 8 do
+    for b = 0 to 6 do
+      for c = 0 to 4 do
+        readln(L[a][b][c]);
+
+  for i = 0 to 8 * 6 * 4 do
+  begin
+    if (i mod 12) = 0 then
+    begin 
+      writeln;
+      for j = 0 to i div 12 do
+        write(' ');
+    end;
+    
+    a := i div 4 * 6;
+    b := (i div 4) mod 6;
+    c := i mod 4;
+    
+  
+    write(L[a][b][c], ' ');
+  end;
+End.
