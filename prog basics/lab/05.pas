88c05ba06047eac9f3b223dae99e585b5ce82bf8
BANO.notIT <bano.notit@yandex.ru>
2019-09-19 10:24:52 +0300

    typos and syntax

--- prog basics/lab/05.pas
+++ prog basics/lab/05.pas
@@ -1,25 +1,27 @@
 program bobrenkoLab05;
 
-var
+Var
   L : array[0..7] of array[0..5] of array[0..3] of integer;
   i, j, a, b, c : integer;
  
 Begin
+
+  writeln('Enter 192 elements :D');
   for a = 0 to 8 do
     for b = 0 to 6 do
       for c = 0 to 4 do
-        readln(L[a][b][c]);
+        read(L[a][b][c]);
 
   for i = 0 to 8 * 6 * 4 do
   begin
     if (i mod 12) = 0 then
-    begin 
+    begin
       writeln;
       for j = 0 to i div 12 do
         write(' ');
     end;
     
-    a := i div 4 * 6;
+    a := i div (4 * 6);
     b := (i div 4) mod 6;
     c := i mod 4;
     
