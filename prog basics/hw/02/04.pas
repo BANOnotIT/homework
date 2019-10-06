22bf02729ee6737b3abf0222884fadf115687f21
BANO.notIT <bano.notit@yandex.ru>
2019-10-06 16:22:05 +0300

    fix: 04 in hw 02

--- prog basics/hw/02/04.pas
+++ prog basics/hw/02/04.pas
@@ -1,23 +1,23 @@
 Uses
-  Trap;
+  Trap, math;
 
 Const
   a1: real = -1;
   b1: real = 4;
   
-  a2: real = -.5;
+  a2: real = -0.5;
   b2: real = 0;
 
 Var
   n, choice : integer;
-  s : real
+  s : real;
 
-function fn1(x : real): real;
+function fn1(const x : real): real;
 begin
-  fn1 := x + cox(x);
+  fn1 := x + cos(x);
 end;
 
-function fn2(x : real): real;
+function fn2(const x : real): real;
 begin
   fn2 := tan(x + 1)/(x + 1);
 end;
@@ -25,7 +25,7 @@ end;
 Begin
 
   repeat
-    writeln('Enter function varian (1 - cos, 2 - sin):');
+    writeln('Enter function variant (1 - cos, 2 - tan):');
     readln(choice);
   until choice in [1, 2];
   
@@ -34,10 +34,11 @@ Begin
     readln(n);
   until n > 0;
   
-  case choice 
-   1: s := TRAP(fn1, a1, b1, n);
-   2: s := TRAP(fn2, a2, b2, n);
+  case choice of
+   1: s := TRAP.Trap(@fn1, a1, b1, n);
+   2: s := TRAP.Trap(@fn2, a2, b2, n);
   end;
    
-  writeln('Area is ', s);
+  writeln('Area is:');
+  writeln(s:10:10);
 End.
--- prog basics/hw/02/Trap.pas
+++ prog basics/hw/02/Trap.pas
@@ -1,26 +1,21 @@
 Unit Trap;
-
-Type
-  TFn: function(x : real): real;
-  
 Interface
-  function TRAP(f : TFn; a, b : real; n : integer): real;
+  Type TFn = function(const x : real): real;
+  function TRAP(const f : TFn; a, b : real; n : integer): real;
   
 Implementation
-  function TRAP(f : TFn; a, b : real; n : integer): real;
+  function TRAP(const f : TFn; a, b : real; n : integer): real;
   var
     h : real;
     i : integer;
-    s : real;
   begin
   
     h := (b - a) / n;
-    
     trap := (f(a) + f(b)) / 2;
     
     for i := 1 to n - 1 do
       trap := trap + f(a + (h * i));
     
     trap := trap * h;
-  
   end;
+end.
