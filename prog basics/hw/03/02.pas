517e7ae555a2c275ac316b0c6ba5e5dc99f767a4
BANO.notIT <bano.notit@yandex.ru>
2019-10-25 01:17:35 +0300

    report hw03

--- prog basics/hw/03/02.pas
+++ prog basics/hw/03/02.pas
@@ -1,10 +1,9 @@
 {$h+} 
 {$mode objfpc} 
-
+// tag::implementation[]
 Type
   TBlock = object
-    protected 
-      width, length, height : real;
+    protected width, length, height : real;
     public
       procedure print; virtual;
       constructor init(w,h,l : real);
@@ -18,62 +17,43 @@ Type
       constructor init(w,h,l,m : real);
       procedure print;
   end;
-  
-  
 constructor TBlock.init(w,h,l : real);
 begin
-  width := w;
-  height := h;
-  length := l;
+  width := w;  height := h;  length := l;
 end;
-
 procedure TBlock.print;
 begin
   write('width:',width:5:5,' height:',height:5:5,' length:',length:5:5);
 end;
-
 function TBlock.getValue() : real;
-begin
-  getValue := width * height * length;
-end;
-
+begin  getValue := width * height * length;  end;
 constructor TBrick.init(w,h,l,m : real);
 begin
   inherited init(w,h,l);
   mass := m;
 end;
-
 procedure TBrick.print();
 begin
   inherited print;
   write(' mass:',mass:5:5);
 end;
-
 function TBrick.getDensity : real;
-begin
-  getDensity := mass / getValue;
-end;
-
+begin  getDensity := mass / getValue;  end;
+// end::implementation[]
+// tag::testing[]
 Var
-  brick : TBrick;
-  block : TBlock;
+  brick : TBrick;  block : TBlock;
 Begin
   brick.init(1,2,3,6);
   block.init(3,2,2);
 
-
-
-  // writeln(block);
-  
-  writeln('TBlock getValue+init test: ',block.getValue() = 12);
-  
+  writeln('TBlock getValue+init test: ',block.getValue() = 12);  
   writeln('TBlock print test (visual) 3,2,2:');
-  block.print;
-  writeln;
+  block.print;  writeln;
   
   writeln('TBrick getValue+init test: ', brick.getValue = 6);
   writeln('TBrick getDensity test: ', 1 = brick.getDensity);
   writeln('TBrick print test (visual) 1,2,3,6: ');
-  brick.print;
-  writeln;
+  brick.print;  writeln;
 End.
+// end::testing[]
--- prog basics/hw/03/03.pas
+++ prog basics/hw/03/03.pas
@@ -1,95 +1,66 @@
-Type
-  sym = string[2];
+// tag::implementation[]
+Type  sym = string[2];
   Atom = object
-    private
-      symbol : sym;
-      mass : real;
-      
+    private  symbol : sym;  mass : real;
     public
       procedure Init(s : sym; m : real);
       procedure print;
       function getMass : real;
       function getSym : sym;
   end;
-
   pAtom = ^Atom;
-  
   Molecule = object
-    private
-      atoms : array of pAtom;
+    private   atoms : array of pAtom;
       ratios : array of integer;
     public
       procedure Init(ats : array of pAtom);
       procedure print;
       function getMass : real;
   end;
-  
 procedure Molecule.Init(ats : array of pAtom);
-var
-  i, l : integer;
+var i, l : integer;
 begin
   setlength(self.atoms, 1);
   setlength(self.ratios, 1);
   self.atoms[0] := ats[0];
   self.ratios[0] := 1;
   l := 1;
-  
   for i := 1 to length(ats)-1 do begin
     if atoms[l-1]^.getSym = ats[i]^.getSym then
       inc(ratios[l-1])
     else begin
       inc(l);
       setlength(self.atoms, l); setlength(self.ratios, l);
-      
       self.atoms[l-1] := ats[i];
       self.ratios[l-1] := 1;
-    end;
-  end;
-end;
-
+end;  end;  end;
 procedure Molecule.print;
-var 
-  i : integer;
+var i : integer;
 begin
   for i := 0 to length(self.atoms)-1 do begin
     write(self.atoms[i]^.getSym);
     if self.ratios[i] <> 1 then
       write(self.ratios[i]);
-  end;
-  writeln;
+  end;   writeln;
 end;
-
 function Molecule.getMass : real;
-var
-  i : integer;
+var i : integer;
 begin
   getMass := 0;
   for i := 0 to length(atoms)-1 do begin
     getMass := getMass + (atoms[i]^).getMass * ratios[i];
   end;
 end;
-
 procedure Atom.Init(s : sym; m:real);
-begin
-  symbol := s;
-  mass := m;
-end;
-
+begin   symbol := s;  mass := m;   end;
 function Atom.getSym : sym;
-begin
-  getSym := symbol;
-end;
-
+begin   getSym := symbol;   end;
 function Atom.getMass : real;
-begin
-  getMass := mass;
-end;
-
+begin   getMass := mass;   end;
 procedure Atom.print;
-begin
-  writeln(symbol, ': ', mass);
-end;
-
+begin   writeln(symbol, ': ', mass);   end;
+// end::implementation[]
+// tag::testing[]
 Var
   Fe, Cl : Atom;
   scheme : array of pAtom;
@@ -99,18 +70,18 @@ Begin
   Fe.Init('Fe', 55.845);
   Cl.Init('Cl', 35.45);
   setlength(scheme, 4);
-  scheme[0] := @Fe;  scheme[1] := @Cl;
-  scheme[2] := @Cl;  scheme[3] := @Cl;
+  scheme[0] := @Fe;   scheme[1] := @Cl;
+  scheme[2] := @Cl;   scheme[3] := @Cl;
   
   writeln('test Atom.Init+.getMass: ', (Cl.getMass - 35.45) < 1e-5);
   writeln('test Atom.getSym: ', Cl.getSym = 'Cl');
   writeln('visual test Atom.print: (Fe: 55.845)');
   Fe.print;
-  
-  FeCl3.Init(scheme);
-  
+
+  FeCl3.Init(scheme);  
   writeln('visual test Molecule.print: (FeCl3)');
   fecl3.print;
   
   writeln('test Molecule.getMass: ',(fecl3.getMass-(35.45*3+55.845)) < 1e-5);
 End.
+// end::testing[]
