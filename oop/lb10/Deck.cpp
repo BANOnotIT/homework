5efe9de4e5ddde9c0a4b23b1b49fea15f028cec1
BANO.notIT <bano.notit@yandex.ru>
2020-03-13 22:49:25 +0300

    doc(lb8) readme

---
+++ oop/lb10/Deck.cpp
@@ -0,0 +1,5 @@
+//
+// Created by banonotit on 13.03.2020.
+//
+
+#include "Deck.h"
---
+++ oop/lb10/Deck.h
@@ -0,0 +1,14 @@
+//
+// Created by banonotit on 13.03.2020.
+//
+
+#ifndef LB10_DECK_H
+#define LB10_DECK_H
+
+
+class Deck {
+
+};
+
+
+#endif //LB10_DECK_H
