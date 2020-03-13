f46a931d225942c747bf8377585be838892b105f
BANO.notIT <bano.notit@yandex.ru>
2020-03-13 21:47:59 +0300

    code(lb6) start

---
+++ oop/lb06/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+struct Item {
+    int val;
+    Item *next;
+};
+
+int main() {
+    cout << "Enter chars" << endl;
+    string input;
+    cin >> input;
+
+    auto start = new Item;
+    auto last = start;
+
+    for (char ch:input) {
+        last->val = ch - 0x30;
+        last->next = new Item;
+        last = last->next;
+    }
+
+
+    int sum(0);
+    int stat[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    auto cur = start;
+    while (cur != nullptr) {
+        sum += cur->val;
+        stat[cur->val]++;
+
+        cout << cur->val << endl;
+
+        cur = cur->next;
+    }
+
+    cout << "Sum: " << sum << endl << endl;
+    cout<<"Table"<<endl;
+    cout.flush();
+
+    for (int i = 0; i < 10; ++i)
+        if (stat[i])
+            printf("%d: %4d\n", i, stat[i]);
+
+    // cleaning up
+    cur = start;
+    Item *tmp;
+    while (cur != nullptr) {
+        tmp = cur;
+        cur = cur->next;
+        delete tmp;
+    }
+}
+/// 1234567876543456789 => 100
