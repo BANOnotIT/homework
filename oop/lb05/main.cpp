944ab041933cff3f802eac91af6bd76f43501641
BANO.notIT <bano.notit@yandex.ru>
2020-03-13 12:42:40 +0300

    repo: reorganize lb5

---
+++ oop/lb05/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter text" << endl;
+    string text;
+    cin >> text;
+
+    bool f(false), s(false), r(false);
+
+    bool *cur = nullptr;
+
+    for (int i = 0; i < text.length(); ++i) {
+        switch (text[i]) {
+            case 'f':
+                cur = &f;
+                break;
+            case 's':
+                cur = &s;
+                break;
+            case 'r':
+                cur = &r;
+                break;
+            default:
+                cur = nullptr;
+        }
+
+        if (cur != nullptr && !*cur) {
+            *cur = true;
+            cout << text[i + 1] << " -> " << static_cast<void *>(&text[i + 1]) << endl;
+        }
+
+        if (f && s && r) break;
+    }
+
+    return 0;
+}
