81786065f55175335e120d129c783e597b7b5199
BANO.notIT <bano.notit@yandex.ru>
2020-11-27 15:04:32 +0300

    test: rss and json support for endpoint

--- web/rails-project/test/controllers/welcome_controller_test.rb
+++ web/rails-project/test/controllers/welcome_controller_test.rb
@@ -13,4 +13,17 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'td', '9' # last result
     assert_select 'td>strong', '4' # count
   end
+
+  test "should return json" do
+    get welcome_result_url, params: {n: 5, format: 'json'}
+    assert_response :success
+
+    assert_includes @response.headers['Content-Type'], 'application/json'
+  end
+  test "should return rss" do
+    get welcome_result_url, params: {n: 5, format: 'rss'}
+    assert_response :success
+
+    assert_includes @response.headers['Content-Type'], 'application/rss'
+  end
 end
