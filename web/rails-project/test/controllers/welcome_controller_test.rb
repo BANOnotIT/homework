247ca59448deaf6ea55644cf61b832f80b8d622c
BANO.notIT <bano.notit@yandex.ru>
2020-11-30 01:25:14 +0300

    style: run auto refactor

--- web/rails-project/test/controllers/welcome_controller_test.rb
+++ web/rails-project/test/controllers/welcome_controller_test.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 require 'json'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get welcome_index_url
     assert_response :success
   end
 
-  test "should get result" do
-    get welcome_result_url, params: {n: 5}
+  test 'should get result' do
+    get welcome_result_url, params: { n: 5 }
     assert_response :success
 
     assert_select 'td', '9' # last result
     assert_select 'td>strong', '4' # count
   end
 
-  test "should return json" do
-    get welcome_result_url, params: {n: 5, format: 'json'}
+  test 'should return json' do
+    get welcome_result_url, params: { n: 5, format: 'json' }
     assert_response :success
 
     assert_includes @response.headers['Content-Type'], 'application/json'
   end
 
-  test "should return rss" do
-    get welcome_result_url, params: {n: 5, format: 'rss'}
+  test 'should return rss' do
+    get welcome_result_url, params: { n: 5, format: 'rss' }
     assert_response :success
 
     assert_includes @response.headers['Content-Type'], 'application/rss'
