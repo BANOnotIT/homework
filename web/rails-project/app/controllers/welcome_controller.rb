a5deed3e19b25e1b42e14b34b744821d9484e332
BANO.notIT <bano.notit@yandex.ru>
2020-11-30 01:24:15 +0300

    code(lb11) implement caching

--- web/rails-project/app/controllers/welcome_controller.rb
+++ web/rails-project/app/controllers/welcome_controller.rb
@@ -2,10 +2,17 @@
 
 class WelcomeController < ApplicationController
   def result
-    @result = (0..params[:n].to_i)
-              .map { |a| { x: a, xsqr: a**2 } }
-              .select { |a| a.values.all? { |x| is_palindrome x } }
-              .to_a
+    n = params[:n].to_i
+
+    cached_result = CachedResult.find_or_initialize_by(n: n)
+
+    if cached_result.new_record?
+      cached_result.values = squared_palindromes n
+      cached_result.save!
+    end
+
+    @result = cached_result.values
+
     respond_to do |format|
       format.json { render json: @result }
       format.html { render }
@@ -16,7 +23,23 @@ class WelcomeController < ApplicationController
 
   def index; end
 
+  def cached
+    result = CachedResult.all.map { |inst| { n: inst.n, result: inst.values } }
+
+    respond_to do |format|
+      format.xml { render xml: result.to_xml }
+      format.json { render json: result }
+    end
+  end
+
   private
+
+  def squared_palindromes(n)
+    (0..n)
+        .map { |a| { x: a, xsqr: a ** 2 } }
+        .select { |a| a.values.all? { |x| is_palindrome x } }
+        .to_a
+  end
 
   def is_palindrome(num)
     string = num.to_s
