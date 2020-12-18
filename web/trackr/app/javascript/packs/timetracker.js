40185be33e717aebcec111a1c3eb2024606cd8dc
BANO.notIT <bano.notit@yandex.ru>
2020-12-18 05:52:09 +0300

    feat: time interval form

--- web/trackr/app/javascript/packs/timetracker.js
+++ web/trackr/app/javascript/packs/timetracker.js
@@ -1,5 +1,8 @@
 import $ from 'jquery'
+import {EntryEditor} from "../EntryEditor";
 
 $(document).on('click', '.js-time-entry .js-group-btn', function () {
     $(this).closest('.js-time-entry').next().slideToggle('fast')
 })
+
+new EntryEditor($('.js-entry-editor'))
