cd4e1dc4c3be7787dd5fdc4516255405aff36dd0
BANO.notIT <bano.notit@yandex.ru>
2020-12-21 04:28:27 +0300

    style: run prettier

--- web/trackr/app/javascript/packs/application.js
+++ web/trackr/app/javascript/packs/application.js
@@ -5,13 +5,12 @@
 
 import $ from "jquery";
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-require("bootstrap-icons/font/bootstrap-icons.css")
-
+require("bootstrap-icons/font/bootstrap-icons.css");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,9 +19,8 @@ require("bootstrap-icons/font/bootstrap-icons.css")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-$(document).on('click', '.js-switch-locale', function () {
-    const locale = $(this).data('locale')
-    document.cookie = `locale=${locale}`
-    window.location.reload()
-})
-
+$(document).on("click", ".js-switch-locale", function () {
+  const locale = $(this).data("locale");
+  document.cookie = `locale=${locale}`;
+  window.location.reload();
+});
